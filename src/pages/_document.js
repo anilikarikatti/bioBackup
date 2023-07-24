81438508f61d60bd8288ccec4dd5e19f999afc99
anilikarikatti <anilkarikatti333@gmail.com>
2023-07-24 18:01:36 +0530

    added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+
       <Head />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body>
         <Main />
         <NextScript />
